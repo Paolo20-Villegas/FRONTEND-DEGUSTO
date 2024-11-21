22c95f636fda31cdaabd9f1e86a843fe7082734c
Paolo <pao_val3107@hotmail.com>
2024-11-21 17:30:18 -0500

    Updates

---
+++ precache-manifest.e4703dbe4b94ea0f80bea5bf746cffc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d14003aa081674dc427e40cd777733d",
+    "url": "/FRONTEND-DEGUSTO/index.html"
+  },
+  {
+    "revision": "1eead8ad4b005ba98760",
+    "url": "/FRONTEND-DEGUSTO/static/css/main.b2e2248c.chunk.css"
+  },
+  {
+    "revision": "8841fd20c4da8a0504dd",
+    "url": "/FRONTEND-DEGUSTO/static/js/2.8dfd44ed.chunk.js"
+  },
+  {
+    "revision": "ea862b4d7291d9d99556",
+    "url": "/FRONTEND-DEGUSTO/static/js/3.bd0f349a.chunk.js"
+  },
+  {
+    "revision": "1eead8ad4b005ba98760",
+    "url": "/FRONTEND-DEGUSTO/static/js/main.df80ade2.chunk.js"
+  },
+  {
+    "revision": "33fa42b06581aa07549f",
+    "url": "/FRONTEND-DEGUSTO/static/js/runtime~main.55ced862.js"
+  }
+]);
